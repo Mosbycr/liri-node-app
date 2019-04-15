9093500a56494332816306bee74a6cc648f203dd
Mosbycr <mosbycr@gmail.com>
2019-04-14 21:35:55 -0400

    switch case set up, code written for spotify but token problem to solve

--- liri.js
+++ liri.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 spotify();
 
 var userRequestAction = process.argv[2];
+var userInput = process.argv.slice(3).join("+");
+console.log(userInput);
 
 switch (userRequestAction){
     case "spotify-this-song":
@@ -11,9 +13,17 @@ switch (userRequestAction){
 }
 
 function spotify() {
-  //var Spotify = require("node-spotify-api");
+  var Spotify = require("node-spotify-api");
   var keys = require("./keys.js");
   var spotify = new Spotify(keys.spotify);
+  console.log(spotify);
 
+  spotify.search({ type: "track", query: "All the Small Things", limit: 1 }, function(err,data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+
+    console.log(data);
+  });
 
 }
