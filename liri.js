0c6ce93a74ef7bf80e93d5450641e043482c40a3
Mosbycr <mosbycr@gmail.com>
2019-04-16 16:16:42 -0400

    trying to find check for bands empty []

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@ var moment = require('moment');
 
 var userRequestAction = process.argv[2];
 var userInput = process.argv.slice(3).join("+"); //will work for spotify?
+var artist = process.argv.slice(3).join("");
 console.log(userInput);
 
 
@@ -15,7 +16,7 @@ switch (userRequestAction){
     break;
 
     case "concert-this":
-    bandsInTown();
+    bandsInTown(artist);
     break;
 
     case "movie-this":
@@ -41,47 +42,29 @@ function spotify() {
 }
 
 
-function bandsInTown(){
-  //variable that takes artists name of any length
-  var artist = process.argv.slice(3).join("");
-  //console.log(artist);
+function bandsInTown(artist){
   var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
   console.log(queryURL);
 
   //need to add some way to say try another band when artist info not retrieved and empty array provided in queryURL - status code will not work both 200 and no error
   axios.get(queryURL).then(function (response) {
-    //  console.log(response);
+    console.log(response);
     var results = response.data;
 
-    //loops through every available event and lists the venue name, location, and date of event
-    for (var i = 0; i < results.length; i++){
-      console.log("\n****** Band Event Search Results ******" +
-      "\nThe Venue: " + results[i].venue.name +
-      "\nThe Venue location is: " + results[i].venue.city + ", " + results[i].venue.region);
-      var date = results[i].datetime;
-      var newDate = moment(date).format("MM/DD/YYYY");
-      console.log("Date of the Event: " + newDate);
+    if (response.data === []){  //does not work for check 
+      console.log("try again");
+    } else {
+      //loops through every available event and lists the venue name, location, and date of event
+      for (var i = 0; i < results.length; i++) {
+        console.log("\n****** Band Event Search Results ******" +
+          "\nThe Venue: " + results[i].venue.name +
+          "\nThe Venue location is: " + results[i].venue.city + ", " + results[i].venue.region);
+        var date = results[i].datetime;
+        var newDate = moment(date).format("MM/DD/YYYY");
+        console.log("Date of the Event: " + newDate);
+      }
     }
-    
-  // }).catch(function (error) {
-  //   if (error.response) {
-  //     // The request was made and the server responded with a status code
-  //     // that falls out of the range of 2xx
-  //     console.log(error.response.data);
-  //     console.log(error.response.status);
-  //     console.log(error.response.headers);
-  //   } else if (error.request) {
-  //     // The request was made but no response was received
-  //     // `error.request` is an object that comes back with details pertaining to the error that occurred.
-  //     console.log(error.request);
-  //   } else {
-  //     // Something happened in setting up the request that triggered an Error
-  //     console.log("Error", error.message);
-  //   }
-  //   console.log(error.config);
   });
-
-
 }
 
 //axios call to omdb api to retrieve requested movie data
